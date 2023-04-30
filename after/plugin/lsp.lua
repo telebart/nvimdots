6f9772b65dfb91d10d33634b075d0f4ef286afe1
telebart <telebart@telebart.telebart>
2023-04-30 07:35:00 +0300

    keybind

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -122,8 +122,8 @@ cmp.setup({
     -- documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
     --['<C-Space>'] = cmp.mapping.complete(),
     --['<CR>'] = cmp.mapping.confirm({ behavior=cmp.ConfirmBehavior.Replace, select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
