322925871eb630f96f4971d1cd4e5a020c73f191
telebart <telebart@telebart.telebart>
2023-07-14 22:06:47 +0300

    autocmds

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -1,6 +1,5 @@
--- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+  group = vim.api.nvim_create_augroup("l_yank_highlight", { clear = true }),
   pattern = '*',
   callback = function()
     vim.highlight.on_yank({
@@ -11,15 +10,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-  group = vim.api.nvim_create_augroup("formatgo", { clear = true }),
+  group = vim.api.nvim_create_augroup("l_auto_format", { clear = true }),
   pattern = {"*.go", "*.js", "*.ts", "*.jsx", "*.tsx", "*.json", "*.graphql", "*.tf"},
   callback = function()
     vim.lsp.buf.format({
         filter = function(client)
-            -- apply whatever logic you want (in this example, we'll only use null-ls)
             return client.name == "null-ls"
         end,
     })
     vim.api.nvim_command('write')
     end ,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("l_spell", { clear = true }),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.spell = true
+  end,
 })
