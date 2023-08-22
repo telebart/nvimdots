8d91fd20ea74d4e43f4f16e3df974bd238cb10f4
telebart <telebart@example.com>
2023-08-22 06:28:43 +0300

    harpoon keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,10 +4,9 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+for  i=1,9 do
+  vim.keymap.set("n", "<leader>"..i, function() ui.nav_file(i) end)
+end
 
 require("harpoon").setup({
     menu = {
