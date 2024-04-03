43c65bb0b989746806200374aeeb166494dd1dc0
telebart <telebart@telebart.com>
2024-04-03 08:16:36 +0300

    andanotherone

--- lua/l/custom/harpoon.lua
+++ lua/l/custom/harpoon.lua
@@ -1,53 +1,21 @@
--- return {
---   {
---     "theprimeagen/harpoon",
---     config = function()
---       local mark = require("harpoon.mark")
---       local ui = require("harpoon.ui")
---
---       vim.keymap.set("n", "<leader>a", mark.add_file)
---       vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
---
---       for  i=1,9 do
---         vim.keymap.set("n", "<leader>"..i, function() ui.nav_file(i) end)
---       end
---
---       require("harpoon").setup({
---         menu = {
---           width = 90,
---         }
---       })
---     end
---   }
--- }
-
 return {
-  {
-    'MeanderingProgrammer/harpoon-core.nvim',
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local mark = require("harpoon-core.mark")
-      local ui = require("harpoon-core.ui")
-
-      vim.keymap.set("n", "<leader>a", mark.add_file)
-      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-      for  i=1,9 do
-        vim.keymap.set("n", "<leader>"..i, function() ui.nav_file(i) end)
-      end
-
-      require('harpoon-core').setup({
-        -- Make existing window active rather than creating a new window
-        use_existing = true,
-        -- Set marks specific to each git branch inside git repository
-        mark_branch = false,
-        -- Use the previous cursor position of marked files when opened
-        use_cursor = true,
-        -- Settings for popup window
-        menu = {
-          width = math.floor(0.70 * vim.o.columns),
-        },
-      })
-    end,
+  "theprimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = function()
+    local harpoon = require("harpoon")
+    local keys = {
+      { "<leader>a", function() harpoon:list():add() end },
+      { "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end },
+    }
+    for  i=1,9 do
+      table.insert(keys, { "<leader>"..i, function() harpoon:list():select(i) end })
+    end
+    return keys
+  end,
+  opts = {
+    settings = {
+      save_on_toggle = true,
+    }
   }
 }
